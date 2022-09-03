e5b78d64deebccce06602f8dcf91bbde405bba05
Tsuyoshi Maeda <oasis5tsuyoshi@gmail.com>
2022-09-04 00:51:06 +0900

    Done sqli part

---
+++ section-17/go.mod
@@ -0,0 +1,5 @@
+module myapp
+
+go 1.19
+
+require github.com/mattn/go-sqlite3 v1.14.15 // indirect
---
+++ section-17/main_sqlite.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+var Db *sql.DB
+
+func main() {
+	Db, _ := sql.Open("sqlite3", "./example.db")
+
+	defer Db.Close()
+
+	cmd := `CREATE TABLE IF NOT EXISTS people(
+		name STRING,
+		age INT
+	)`
+
+	_, err := Db.Exec(cmd)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	Insert(Db)
+}
+
+func Insert(db *sql.DB) {
+	cmd := "INSERT INTO people (name, age) VALUES (?, ?)"
+	_, err := db.Exec(cmd, "Taro", 20)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
